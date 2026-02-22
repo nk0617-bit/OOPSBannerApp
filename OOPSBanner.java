2d2be981031bdd5ba6653d6190d9561413923045
nk0617-bit <nk0617@srmist.edu.in>
2026-02-22 23:38:24 +0530

    Saving and Updating Code

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,11 +1,60 @@
 public class OOPSBanner {
+
+    // Method to generate the pattern for the letter 'O'
+    public static String[] getOPattern() {
+        return new String[] {
+            "  ***  ",
+            " **  ** ",
+            "**    **",
+            "**    **",
+            "**    **",
+            " **  ** ",
+            "  ***  "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'P'
+    public static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "**   **",
+            "**   **",
+            "****** ",
+            "**     ",
+            "**     ",
+            "**     "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'S'
+    public static String[] getSPattern() {
+        return new String[] {
+            " ******",
+            "**     ",
+            "**     ",
+            " ***** ",
+            "     **",
+            "     **",
+            "****** "
+        };
+    }
+
+    // Main method to run the banner display
     public static void main(String[] args) {
-        System.out.println("  *****  " + " " + "  *****  " + " " + "******** " + " " + " ********");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + "**     **" + " " + "**       ");
-        System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
-        System.out.println("**     **" + " " + "**     **" + " " + "******** " + " " + " ********");
-        System.out.println("**     **" + " " + "**     **" + " " + "**       " + " " + "       **");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + "       **");
-        System.out.println("  *****  " + " " + "  *****  " + " " + "**       " + " " + "*********");
+
+        // Get patterns for each letter
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Loop to assemble and print "OOPS"
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+            );
+        }
     }
 }
